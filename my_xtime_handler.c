aacea97ecb9d4721303ae518d3a4693273c3c3b5
vedranade <20063646+vedranade@users.noreply.github.com>
2017-10-09 14:07:48 -0400

    Add files via upload

---
+++ my_xtime_handler.c
@@ -0,0 +1,11 @@
+#include <linux/linkage.h>
+#include <linux/export.h>
+#include <linux/time.h>
+#include <asm/uaccess.h>
+#include <linux/printk.h>
+#include <linux/slab.h>
+asmlinkage int sys_my_xtime(void){
+        printk("Time in nanoseconds is: %ld", timespec.tv_nsec);
+        return 0;
+}
+EXPORT_SYMBOL(sys_my_xtime);
